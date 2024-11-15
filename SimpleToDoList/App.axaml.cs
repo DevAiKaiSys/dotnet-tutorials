34320d197c28d1cca0580ef968f99f3985f7c970
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-15 21:42:08 +0700

    Step 6 (Optional): Data Persistence

--- SimpleToDoList/App.axaml.cs
+++ SimpleToDoList/App.axaml.cs
@@ -1,21 +1,32 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using SimpleToDoList.Services;
 using SimpleToDoList.ViewModels;
 using SimpleToDoList.Views;
 
 namespace SimpleToDoList;
 
-public partial class App : Application
+public class App : Application
 {
+    // This is a reference to our MainViewModel which we use to save the list on shutdown. You can also use Dependency Injection
+// in your App.
+    private readonly MainWindowViewModel _mainViewModel = new();
+
+    // We want to save our ToDoList before we actually shutdown the App. As File I/O is async, we need to wait until file is closed
+// before we can actually close this window
+
+    private bool _canClose; // This flag is used to check if window is allowed to close
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
@@ -24,10 +35,44 @@ public partial class App : Application
             BindingPlugins.DataValidators.RemoveAt(0);
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext =
+                    _mainViewModel // Remember to change this line to use our private reference to the MainViewModel
             };
+
+            // Listen to the ShutdownRequested-event
+            desktop.ShutdownRequested += DesktopOnShutdownRequested;
         }
 
         base.OnFrameworkInitializationCompleted();
+
+        // Init the MainViewModel 
+        await InitMainViewModelAsync();
+    }
+
+    private async void DesktopOnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+    {
+        e.Cancel = !_canClose; // cancel closing event first time
+
+        if (!_canClose)
+        {
+            // To save the items, we map them to the ToDoItem-Model which is better suited for I/O operations
+            var itemsToSave = _mainViewModel.ToDoItems.Select(item => item.GetToDoItem());
+            await ToDoListFileService.SaveToFileAsync(itemsToSave);
+
+            // Set _canClose to true and Close this Window again
+            _canClose = true;
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) desktop.Shutdown();
+        }
+    }
+
+    // Optional: Load data from disc
+    private async Task InitMainViewModelAsync()
+    {
+        // get the items to load
+        var itemsLoaded = await ToDoListFileService.LoadFromFileAsync();
+
+        if (itemsLoaded is not null)
+            foreach (var item in itemsLoaded)
+                _mainViewModel.ToDoItems.Add(new ToDoItemViewModel(item));
     }
 }
