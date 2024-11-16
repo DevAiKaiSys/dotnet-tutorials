8e551348fb4cafc0102011c6e8a503c4ae63a49f
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-16 23:57:04 +0700

    Finishing the App

--- XAML-MVUX/SimpleCalculator/MainModel.cs
+++ XAML-MVUX/SimpleCalculator/MainModel.cs
@@ -1,10 +1,20 @@
 namespace SimpleCalculator;
 public partial record MainModel
 {
-    public MainModel()
+    public MainModel(IThemeService themeService)
     {
         Calculator = State.Value(this, () => new Calculator());
-        IsDark = State<bool>.Value(this, () => false);
+        IsDark = State<bool>.Value(this, () => themeService.IsDark);
+
+        themeService.ThemeChanged += async (_, _) =>
+        {
+            // Retrieve the IsDark property whilst still on the UI thread
+            bool isDark = themeService.IsDark;
+            await IsDark.Update(_ => isDark, CancellationToken.None);
+        };
+
+        _ = IsDark.ForEachAsync(async (dark, ct) =>
+            await themeService.SetThemeAsync(dark ? AppTheme.Dark : AppTheme.Light));
     }
 
     public IState<bool> IsDark { get; }
