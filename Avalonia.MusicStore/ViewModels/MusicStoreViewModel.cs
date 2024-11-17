6c3e734ccfbee72d93f7838c60229a9e35451c35
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-18 00:03:11 +0700

    Mock Search Reactive View Model

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,21 @@
+using ReactiveUI;
+
 namespace Avalonia.MusicStore.ViewModels;
 
-public class MusicStoreViewModel
+public class MusicStoreViewModel : ViewModelBase
 {
-    
+    private bool _isBusy;
+    private string? _searchText;
+
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
 }
