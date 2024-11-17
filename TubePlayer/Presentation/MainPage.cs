7808957c77cbc2f08c0a96d2a4ee67d64ff831a9
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-17 18:05:39 +0700

    Bind UI

--- TubePlayer/Presentation/MainPage.cs
+++ TubePlayer/Presentation/MainPage.cs
@@ -1,23 +1,37 @@
-﻿namespace TubePlayer.Presentation;
+﻿using Microsoft.UI.Text;
+using TubePlayer.Business.Models;
+
+namespace TubePlayer.Presentation;
 
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this.NavigationCacheMode(NavigationCacheMode.Required)
+        this.DataContext<MainViewModel>((page, vm) => page
+            .NavigationCacheMode(NavigationCacheMode.Required)
             .Background(Theme.Brushes.Background.Default)
-            .Content(
-                new Grid()
-                    .SafeArea(SafeArea.InsetMask.All)
-                    .RowDefinitions("Auto, *")
-                    .Children(
-                        new TextBox()
-                            .PlaceholderText("Search term"),
-                        new ListView()
-                            .Grid(row: 1)
-                            .ItemsSource(new[] { "Avatar", "Titanic", "Star Wars" })
-                            .ItemTemplate<string>(videoTitle =>
-                                new TextBlock()
-                                    .Text(() => videoTitle))));
+            .Content(new Grid()
+                .SafeArea(SafeArea.InsetMask.All)
+                .RowDefinitions("Auto, *")
+                .Children(
+                    new TextBox()
+                        .Text(x => x
+                            .Binding(() => vm.SearchTerm)
+                            .Mode(BindingMode.TwoWay)
+                            .UpdateSourceTrigger(UpdateSourceTrigger.PropertyChanged))
+                        .PlaceholderText("Search term"),
+                    new ListView()
+                        .Grid(row: 1)
+                        .ItemsSource(() => vm.VideoSearchResults)
+                        .ItemTemplate<YoutubeVideo>(ytv =>
+                            new StackPanel()
+                                .Children(
+                                    new TextBlock()
+                                        .FontWeight(FontWeights.Bold)
+                                        .Text(() =>
+                                            ytv.Details.Snippet?.ChannelTitle),
+                                    new TextBlock()
+                                        .Text(() =>
+                                            ytv.Details.Snippet?.Title))))));
     }
 }
