74e71ad7b997dd8fddc8e85164eaf14e98b70be6
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-17 16:51:58 +0700

    Module 2 - Creating basic UI layout with C# markup

--- TubePlayer/Presentation/MainPage.cs
+++ TubePlayer/Presentation/MainPage.cs
@@ -4,27 +4,20 @@ public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this.DataContext<MainViewModel>((page, vm) => page
-            .NavigationCacheMode(NavigationCacheMode.Required)
+        this.NavigationCacheMode(NavigationCacheMode.Required)
             .Background(Theme.Brushes.Background.Default)
-            .Content(new Grid()
-                .SafeArea(SafeArea.InsetMask.VisibleBounds)
-                .RowDefinitions("Auto,*")
-                .Children(
-                    new NavigationBar().Content(() => vm.Title),
-                    new StackPanel()
-                        .Grid(row: 1)
-                        .HorizontalAlignment(HorizontalAlignment.Center)
-                        .VerticalAlignment(VerticalAlignment.Center)
-                        .Spacing(16)
-                        .Children(
-                            new TextBox()
-                                .Text(x => x.Binding(() => vm.Name).Mode(BindingMode.TwoWay))
-                                .PlaceholderText("Enter your name:"),
-                            new Button()
-                                .Content("Go to Second Page")
-                                .AutomationProperties(automationId: "SecondPageButton")
-                                .Command(() => vm.GoToSecond)
-                                ))));
+            .Content(
+                new Grid()
+                    .SafeArea(SafeArea.InsetMask.All)
+                    .RowDefinitions("Auto, *")
+                    .Children(
+                        new TextBox()
+                            .PlaceholderText("Search term"),
+                        new ListView()
+                            .Grid(row: 1)
+                            .ItemsSource(new[] { "Avatar", "Titanic", "Star Wars" })
+                            .ItemTemplate<string>(videoTitle =>
+                                new TextBlock()
+                                    .Text(() => videoTitle))));
     }
 }
