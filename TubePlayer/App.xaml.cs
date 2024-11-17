ed4313341bd0ee4688e0f6d8585d5a99757eb048
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-17 16:12:30 +0700

    Add project files.

---
+++ TubePlayer/App.xaml.cs
@@ -0,0 +1,77 @@
+using Uno.Resizetizer;
+
+namespace TubePlayer;
+public partial class App : Application
+{
+    /// <summary>
+    /// Initializes the singleton application object. This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected Window? MainWindow { get; private set; }
+    protected IHost? Host { get; private set; }
+
+    protected async override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        // Load WinUI Resources
+        Resources.Build(r => r.Merged(
+            new XamlControlsResources()));
+
+        // Load Uno.UI.Toolkit and Material Resources
+        Resources.Build(r => r.Merged(
+            new MaterialToolkitTheme(
+                    new Styles.ColorPaletteOverride(),
+                    new Styles.MaterialFontsOverride())));
+        var builder = this.CreateBuilder(args)
+            // Add navigation support for toolkit controls such as TabBar and NavigationView
+            .UseToolkitNavigation()
+            .Configure(host => host
+#if DEBUG
+                // Switch to Development environment when running in DEBUG
+                .UseEnvironment(Environments.Development)
+#endif
+                .UseConfiguration(configure: configBuilder =>
+                    configBuilder
+                        .EmbeddedSource<App>()
+                        .Section<AppConfig>()
+                )
+                .ConfigureServices((context, services) =>
+                {
+                    // TODO: Register your services
+                    //services.AddSingleton<IMyService, MyService>();
+                })
+                .UseNavigation(ReactiveViewModelMappings.ViewModelMappings, RegisterRoutes)
+            );
+        MainWindow = builder.Window;
+
+#if DEBUG
+        MainWindow.UseStudio();
+#endif
+        MainWindow.SetWindowIcon();
+
+        Host = await builder.NavigateAsync<Shell>();
+    }
+
+    private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
+    {
+        views.Register(
+            new ViewMap(ViewModel: typeof(ShellModel)),
+            new ViewMap<MainPage, MainModel>(),
+            new DataViewMap<SecondPage, SecondModel, Entity>()
+        );
+
+        routes.Register(
+            new RouteMap("", View: views.FindByViewModel<ShellModel>(),
+                Nested:
+                [
+                    new ("Main", View: views.FindByViewModel<MainModel>(), IsDefault:true),
+                    new ("Second", View: views.FindByViewModel<SecondModel>()),
+                ]
+            )
+        );
+    }
+}
