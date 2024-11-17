8e5f91daa6b8737514c4d14d26e154ca7828b543
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-17 21:50:44 +0700

    Module 9 - FeedView none and error templates Extract the item template into a separate method

--- TubePlayer/Presentation/MainPage.cs
+++ TubePlayer/Presentation/MainPage.cs
@@ -76,106 +76,89 @@ public partial class MainPage : Page
                                 .Padding(12, 8)
                                 .Navigation(request: "VideoDetails")
                                 .AutoLayout(primaryAlignment: AutoLayoutPrimaryAlignment.Stretch)
-                                .ItemTemplate<YoutubeVideo>
-                                (
-                                    youtubeVideo =>
-                                        new CardContentControl()
-                                            .Margin(0, 0, 0, 8)
-                                            .Style(StaticResource.Get<Style>("ElevatedCardContentControlStyle"))
-                                            .Content
-                                            (
-                                                new AutoLayout()
-                                                    .Background(Theme.Brushes.Surface.Default)
-                                                    .CornerRadius(12)
-                                                    .PrimaryAxisAlignment(AutoLayoutAlignment.Center)
-                                                    .Children
-                                                    (
-                                                        new AutoLayout()
-                                                            .Background(Theme.Brushes.Surface.Default)
-                                                            .CornerRadius(12)
-                                                            .Padding(8, 8, 8, 0)
-                                                            .MaxHeight(288)
-                                                            .MaxWidth(456)
-                                                            .AutoLayout(AutoLayoutAlignment.Center)
-                                                            .Children
-                                                            (
-                                                                new Border()
-                                                                    .Height(204.75)
-                                                                    .CornerRadius(6)
-                                                                    .Child
-                                                                    (
-                                                                        new Image()
-                                                                            .Source(() =>
-                                                                                youtubeVideo.Details.Snippet?.Thumbnails
-                                                                                    ?.Medium?.Url!)
-                                                                            .Stretch(Stretch.UniformToFill)
-                                                                    ),
-                                                                new AutoLayout()
-                                                                    .Spacing(8)
-                                                                    .Orientation(Orientation.Horizontal)
-                                                                    .Padding(0, 8)
-                                                                    .Children
-                                                                    (
-                                                                        new Border()
-                                                                            .Width(60)
-                                                                            .Height(60)
-                                                                            .CornerRadius(6)
-                                                                            .AutoLayout(
-                                                                                AutoLayoutAlignment
-                                                                                    .Center)
-                                                                            .Child
-                                                                            (
-                                                                                new Image()
-                                                                                    .Source(() =>
-                                                                                        youtubeVideo.Channel.Snippet
-                                                                                            ?.Thumbnails?.Medium?.Url!)
-                                                                                    .Stretch(Stretch.UniformToFill)
-                                                                            ),
-                                                                        new AutoLayout()
-                                                                            .PrimaryAxisAlignment(AutoLayoutAlignment
-                                                                                .Center)
-                                                                            .AutoLayout(
-                                                                                primaryAlignment:
-                                                                                AutoLayoutPrimaryAlignment.Stretch)
-                                                                            .Children
-                                                                            (
-                                                                                new TextBlock()
-                                                                                    .Text(() => youtubeVideo.Channel
-                                                                                        .Snippet?.Title)
-                                                                                    .Height(22)
-                                                                                    .Foreground(Theme.Brushes.OnSurface
-                                                                                        .Default)
-                                                                                    .Style(Theme.TextBlock.Styles
-                                                                                        .TitleMedium),
-                                                                                new TextBlock()
-                                                                                    .Text(() => youtubeVideo.Details
-                                                                                        .Snippet?.Title)
-                                                                                    .Height(16)
-                                                                                    .Foreground(Theme.Brushes.OnSurface
-                                                                                        .Medium)
-                                                                            ),
-                                                                        new Button()
-                                                                            .Foreground(Theme.Brushes.OnSurface.Variant
-                                                                                .Default)
-                                                                            .Style(Theme.Button.Styles.Icon)
-                                                                            .AutoLayout(
-                                                                                AutoLayoutAlignment
-                                                                                    .Center)
-                                                                            .Content
-                                                                            (
-                                                                                new PathIcon()
-                                                                                    .Data(StaticResource.Get<Geometry>(
-                                                                                        "Icon_Chevron_Right"))
-                                                                                    .Foreground(Theme.Brushes.OnSurface
-                                                                                        .Variant.Default)
-                                                                            )
-                                                                    )
-                                                            )
-                                                    )
-                                            )
-                                )
+                                .ItemTemplate<YoutubeVideo>(VideoItemTemplate)
                         )
                 ))
             ;
+    }
+
+    private static UIElement VideoItemTemplate(YoutubeVideo youtubeVideo)
+    {
+        return new CardContentControl()
+            .Margin(0, 0, 0, 8)
+            .Style(StaticResource.Get<Style>("ElevatedCardContentControlStyle"))
+            .Content
+            (
+                new AutoLayout()
+                    .Background(Theme.Brushes.Surface.Default)
+                    .CornerRadius(12)
+                    .PrimaryAxisAlignment(AutoLayoutAlignment.Center)
+                    .Children
+                    (
+                        new AutoLayout()
+                            .Background(Theme.Brushes.Surface.Default)
+                            .CornerRadius(12)
+                            .Padding(8, 8, 8, 0)
+                            .MaxHeight(288)
+                            .MaxWidth(456)
+                            .AutoLayout(AutoLayoutAlignment.Center)
+                            .Children
+                            (
+                                new Border()
+                                    .Height(204.75)
+                                    .CornerRadius(6)
+                                    .Child
+                                    (
+                                        new Image()
+                                            .Source(() => youtubeVideo.Details.Snippet?.Thumbnails?.Medium?.Url!)
+                                            .Stretch(Stretch.UniformToFill)
+                                    ),
+                                new AutoLayout()
+                                    .Spacing(8)
+                                    .Orientation(Orientation.Horizontal)
+                                    .Padding(0, 8)
+                                    .Children
+                                    (
+                                        new Border()
+                                            .Width(60)
+                                            .Height(60)
+                                            .CornerRadius(6)
+                                            .AutoLayout(AutoLayoutAlignment.Center)
+                                            .Child
+                                            (
+                                                new Image()
+                                                    .Source(() =>
+                                                        youtubeVideo.Channel.Snippet?.Thumbnails?.Medium?.Url!)
+                                                    .Stretch(Stretch.UniformToFill)
+                                            ),
+                                        new AutoLayout()
+                                            .PrimaryAxisAlignment(AutoLayoutAlignment.Center)
+                                            .AutoLayout(primaryAlignment: AutoLayoutPrimaryAlignment.Stretch)
+                                            .Children
+                                            (
+                                                new TextBlock()
+                                                    .Text(() => youtubeVideo.Channel.Snippet?.Title)
+                                                    .Height(22)
+                                                    .Foreground(Theme.Brushes.OnSurface.Default)
+                                                    .Style(Theme.TextBlock.Styles.TitleMedium),
+                                                new TextBlock()
+                                                    .Text(() => youtubeVideo.Details.Snippet?.Title)
+                                                    .Height(16)
+                                                    .Foreground(Theme.Brushes.OnSurface.Medium)
+                                            ),
+                                        new Button()
+                                            .Foreground(Theme.Brushes.OnSurface.Variant.Default)
+                                            .Style(Theme.Button.Styles.Icon)
+                                            .AutoLayout(AutoLayoutAlignment.Center)
+                                            .Content
+                                            (
+                                                new PathIcon()
+                                                    .Data(StaticResource.Get<Geometry>("Icon_Chevron_Right"))
+                                                    .Foreground(Theme.Brushes.OnSurface.Variant.Default)
+                                            )
+                                    )
+                            )
+                    )
+            );
     }
 }
