a86ae0133a29a912e88305be966d93d7e572c4c8
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-07-24 23:19:47 +0700

    Album Service

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,17 +1,12 @@
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia.MusicStore.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Avalonia.MusicStore.ViewModels;
 
 public partial class MusicStoreViewModel : ViewModelBase
 {
-    public MusicStoreViewModel()
-    {
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
-    }
-
     [ObservableProperty]
     public partial string? SearchText { get; set; }
 
@@ -22,4 +17,25 @@ public partial class MusicStoreViewModel : ViewModelBase
     public partial AlbumViewModel? SelectedAlbum { get; set; }
 
     public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+    private async Task DoSearch(string? term)
+    {
+        IsBusy = true;
+        SearchResults.Clear();
+
+        var albums = await Album.SearchAsync(term);
+
+        foreach (var album in albums)
+        {
+            var vm = new AlbumViewModel(album);
+            SearchResults.Add(vm);
+        }
+
+        IsBusy = false;
+    }
+
+    partial void OnSearchTextChanged(string value)
+    {
+        _ = DoSearch(SearchText);
+    }
 }
