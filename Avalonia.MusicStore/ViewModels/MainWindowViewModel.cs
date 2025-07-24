597638976b909316303141553368ff96c8f616a2
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-07-24 23:00:42 +0700

    Button Command

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Avalonia.MusicStore.ViewModels;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Avalonia.MusicStore.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public MainWindowViewModel()
+    {
+        // ViewModel initialization logic.
+    }
+
+    [RelayCommand]
+    private async Task AddAlbumAsync()
+    {
+        // Code here will be executed when the button is clicked.
+    }
 }
