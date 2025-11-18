fddf1a183f5a78c5baf1f7912a7a118bc68ba63e
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-11-19 06:47:22 +0700

    .NET API Middleware for common functionality (Part 5 of 8) | Backend Web Development with .NET for Beginners
    Use built-in middleware, URL Rewrite Middleware

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
 
 var todos = new List<Todo>();
 
