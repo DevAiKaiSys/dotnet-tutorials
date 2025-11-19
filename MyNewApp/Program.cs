85eb9dae2bd67114d4a7171f2a441f0388db02f4
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-11-19 22:16:22 +0700

    Dependency injection for .NET APIs (Part 7 of 8) | Backend Web Development with .NET for Beginners Define an interface and create a concrete implementation of the tasks service Register the service in the service container Use the service Validate that app still works

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ITaskService>(new InMemoryTaskService());
+
 var app = builder.Build();
 
 app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
@@ -14,19 +17,19 @@ app.Use(async (context, next) =>
 
 var todos = new List<Todo>();
 
-app.MapGet("/todos", () => todos);
+app.MapGet("/todos", (ITaskService service) => service.GetTodos());
 
-app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id, ITaskService service) =>
 {
-    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    var targetTodo = service.GetTodoById(id);
     return targetTodo is not null
         ? TypedResults.Ok(targetTodo)
         : TypedResults.NotFound();
 });
 
-app.MapPost("/todos", (Todo task) =>
+app.MapPost("/todos", (Todo task, ITaskService service) =>
 {
-    todos.Add(task);
+    service.AddTodo(task);
     return TypedResults.Created($"/todos/{task.Id}", task);
 })
 .AddEndpointFilter(async (context, next) =>
@@ -50,12 +53,46 @@ app.MapPost("/todos", (Todo task) =>
     return await next(context);
 });
 
-app.MapDelete("/todos/{id}", (int id) =>
+app.MapDelete("/todos/{id}", (int id, ITaskService service) =>
 {
-    todos.RemoveAll(t => id == t.Id);
+    service.DeleteTodoById(id);
     return TypedResults.NoContent();
 });
 
 app.Run();
 
 public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
+
+interface ITaskService
+{
+    Todo? GetTodoById(int id);
+    List<Todo> GetTodos();
+    void DeleteTodoById(int id);
+    Todo AddTodo(Todo task);
+}
+
+class InMemoryTaskService : ITaskService
+{
+    private readonly List<Todo> _todos = [];
+
+    public Todo AddTodo(Todo task)
+    {
+        _todos.Add(task);
+        return task;
+    }
+
+    public void DeleteTodoById(int id)
+    {
+        _todos.RemoveAll(task => task.Id == id);
+    }
+
+    public Todo? GetTodoById(int id)
+    {
+        return _todos.SingleOrDefault(t => t.Id == id);
+    }
+
+    public List<Todo> GetTodos()
+    {
+        return _todos;
+    }
+}
