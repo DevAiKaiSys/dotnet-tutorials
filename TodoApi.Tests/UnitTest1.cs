5d5723a9bf87a231febdd978a3b791135c04a638
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-11-19 23:19:49 +0700

    Add Unit test code

---
+++ TodoApi.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace TodoApi.Tests;
+
+public class UnitTest1
+{
+    private TodoDb CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<TodoDb>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        return new TodoDb(options);
+    }
+
+    [Fact]
+    public async Task GetAllTodos_ReturnsOkOfTodosResult()
+    {
+        // Arrange
+        var db = CreateDbContext();
+
+        // Act
+        var result = await TodosApi.GetAllTodos(db);
+
+        // Assert: Check for the correct returned type
+        Assert.IsType<Ok<Todo[]>>(result);
+    }
+}
