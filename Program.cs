66efd32654d90c9618216636464233d20c64322b
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-11-20 20:48:54 +0700

    Register the database context

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
+// <snippet_UseSwagger>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +23,7 @@ if (app.Environment.IsDevelopment())
        options.DocumentPath = "/openapi/v1.json";
    });
 }
+// </snippet_UseSwagger>
 
 app.UseHttpsRedirection();
 
