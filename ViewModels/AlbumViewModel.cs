e72045d503d603a6194ad5557a69e32fa2add32e
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-02-14 11:29:04 +0700

    Album Service Apple Web API Package Start the Search Bind the Album View

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -1,5 +1,27 @@
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using Avalonia.MusicStore.Models;
+
 namespace Avalonia.MusicStore.ViewModels;
 
 public class AlbumViewModel : ViewModelBase
 {
+    private readonly Album _album;
+
+    public AlbumViewModel(Album album)
+    {
+        _album = album;
+    }
+
+    public string Artist => _album.Artist;
+
+    public string Title => _album.Title;
+
+    public Task<Bitmap?> Cover => LoadCoverAsync();
+
+    // this will be implemented later
+    private async Task<Bitmap?> LoadCoverAsync()
+    {
+        return null;
+    }
 }
