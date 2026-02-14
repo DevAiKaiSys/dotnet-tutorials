fa8fbfaf1497db8ebdf154cee880326e1fc65042
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-02-14 08:27:49 +0700

    Button Icon Button Command

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Avalonia.MusicStore.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public MainViewModel()
+    {
+        // ViewModel initialization logic.
+    }
+    
+    [RelayCommand]
+    private async Task AddAlbumAsync()
+    {
+        // Code here will be executed when the button is clicked.
+    }
+}
